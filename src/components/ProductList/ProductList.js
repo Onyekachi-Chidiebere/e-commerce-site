1e685b87452f4945eb3505e5110cda5e90efe554
Chidiebere <onyekachisamuel18@gmail.com>
2020-07-06 23:25:02 +0100

    created product objects

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,24 +1,37 @@
-import React, { Component } from "react";
-import Title from "./../Title/Title";
-import Product from "../Product/Product";
-import classes from "./ProductList.css";
-import { ProductConsumer } from "./../../context";
-export default class ProductList extends Component {
-  state = {
-    products: []
-  };
-  render() {
-    return (
-      <div className={classes.PageContainer}>
-        <Title name="Suitcase" />
-        <div className ={classes.ProductContainer}>
-          <ProductConsumer>
-            {(value)=>{
-                return value.products.map(product=>(<Product key={product.id} product={product}/>))
-            }}
-          </ProductConsumer>
-        </div>
-      </div>
-    );
-  }
-}
+import iphone from '../../assets/img/apple.jpeg';
+import applewatch from '../../assets/img/applewatch.jpeg';
+import applewatchwhite from '../../assets/img/applewatchwhite.jpeg';
+import babies from '../../assets/img/babies.jpeg';
+import chainwatch from '../../assets/img/chainwatch.jpeg';
+import girls from '../../assets/img/girls.jpeg';
+import infinix from '../../assets/img/infinix.jpeg';
+import itel from '../../assets/img/itel.jpeg';
+import polo from '../../assets/img/Polo.jpeg';
+import SamsungS8 from '../../assets/img/SamsungS8.jpeg';
+import singlet from '../../assets/img/singlet.jpeg';
+import sneakersarsh from '../../assets/img/sneakersarsh.jpg';
+import sneakersblack from '../../assets/img/sneakersblack.jpg';
+import sneakerswhite from '../../assets/img/sneakerswhite.jpg';
+import trouser from '../../assets/img/trouser.jpg';
+
+
+const Products = [
+  {id:'1',category:'phone',tilte:'Samusung',price:'1000',image:SamsungS8},
+  {id:'2',category:'phone',tilte:'Itel',price:'700',image:itel},
+  {id:'3',category:'phone',tilte:'Apple',price:'1200',image:iphone},
+  {id:'4',category:'phone',tilte:'Infinix',price:'800',image:infinix},
+  {id:'11',category:'cloth',tilte:'Babies',price:'1000',image:babies},
+  {id:'12',category:'cloth',tilte:'Trouser',price:'750',image:trouser},
+  {id:'13',category:'cloth',tilte:'Ladies',price:'1200',image:girls},
+  {id:'14',category:'cloth',tilte:'Polo',price:'800',image:polo},
+  {id:'15',category:'cloth',tilte:'Singlet',price:'400',image:singlet},
+  {id:'111',category:'watch',tilte:'Apple',price:'1000',image:applewatch},
+  {id:'112',category:'watch',tilte:'Apple-white',price:'750',image:applewatchwhite},
+  {id:'113',category:'watch',tilte:'chain watch',price:'1200',image:chainwatch},
+  {id:'114',category:'watch',tilte:'Polo',price:'800',image:polo},
+  {id:'1112',category:'shoes',tilte:'sneakers white',price:'1750',image:sneakerswhite},
+  {id:'1113',category:'shoes',tilte:'Sneakers Black',price:'1200',image:sneakersblack},
+  {id:'1114',category:'shoes',tilte:'Sneakers Arsh',price:'1800',image:sneakersarsh},
+];
+
+export default Products;
